6d74961428bdd55429d6a60729e4704ac0530777
aidanch1 <aidanchen@gmail.com>
2020-11-28 13:47:58 -0800

    meh UI and improved usability

--- src/main/resources/edu/uw/sudoku_solver/webpage/js/index.js
+++ src/main/resources/edu/uw/sudoku_solver/webpage/js/index.js
@@ -1,11 +1,14 @@
 var inputboard = document.getElementById("board");
+const BACKGROUND_COLOR = "lightgoldenrodyellow"; // can play around with this
 //initialize board
 for (var i=0; i<9; i++){
     var nextRow = document.createElement("tr");
     for (var j=0; j<9; j++){
         var cursquare = document.createElement("td");
+        cursquare.style.backgroundColor = BACKGROUND_COLOR;
         var numinput = document.createElement("input");
         numinput.type = "text"; 
+        numinput.className = "numinput";
         numinput.size = "1"; // visual purposes
         numinput.maxLength = "1"; // makes it so user can only type 1 character (1 digit number)
         numinput.onkeypress = function(event){
@@ -43,8 +46,24 @@ function solve(){
     	console.log(json);
     	for (var i=0; i<9; i++){
 	        for (var j=0; j<9; j++){
-	            inputboard.rows[i].cells[j].childNodes[0].value = solved[i][j];
+                var cursquare = inputboard.rows[i].cells[j];
+                if (cursquare.childNodes[0].value != 0){
+                    cursquare.style.backgroundColor = "gray";
+                } else {
+                    cursquare.childNodes[0].value = solved[i][j];
+                }
+                cursquare.childNodes[0].readOnly = true;
 	        }
         }
     }).catch(e => console.log(e));
+}
+
+function clearBoard(){
+    for (var i=0; i<9; i++){
+        for (var j=0; j<9; j++){
+            inputboard.rows[i].cells[j].style.backgroundColor = BACKGROUND_COLOR;
+            inputboard.rows[i].cells[j].childNodes[0].value = "";
+            inputboard.rows[i].cells[j].childNodes[0].readOnly = false;
+        }
+    }
 }
