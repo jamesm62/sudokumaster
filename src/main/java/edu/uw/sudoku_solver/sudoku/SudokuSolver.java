273a6d2f6b51dad3abac5925e3c82bb7b7aec6b0
jamesm62 <74880285+jamesm62@users.noreply.github.com>
2020-11-27 16:12:40 -0800

    Solves and generates sudoku puzzles

--- src/main/java/edu/uw/sudoku_solver/sudoku/SudokuSolver.java
+++ src/main/java/edu/uw/sudoku_solver/sudoku/SudokuSolver.java
@@ -1,17 +1,19 @@
-package edu.uw.sudoku_solver.sudoku;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class SudokuSolver {
+	// stores the data of an unsolved sudoku puzzle
+	private int[][] puzzle;
 	
-	private int[][] board;
-	
-	public SudokuSolver(int[][] board) {
-		this.board = board;
+	// pre: puzzle must be a 9x9 array where empty blanks are represented as zero
+	// post: constructs an instance of SudokuSolver with the given unsolved puzzle data
+	public SudokuSolver(int[][] puzzle) {
+		this.puzzle = puzzle;
 	}
 	
+	// returns a 2D array with the data for the puzzle's solution or null if there is no solution
 	public int[][] solve() {
 		List<Set<Integer>> rows = new LinkedList<Set<Integer>>();
 		List<Set<Integer>> cols = new LinkedList<Set<Integer>>();
@@ -23,31 +25,32 @@ public class SudokuSolver {
 		}
 		for (int row = 0; row < 9; row++) {
 			for (int col = 0; col < 9; col++) {
-				if (board[row][col] != 0) {
-					rows.get(row).add(board[row][col]);
-					cols.get(col).add(board[row][col]);
-					squares.get(row/3*3 + col/3).add(board[row][col]);
+				if (puzzle[row][col] != 0) {
+					rows.get(row).add(puzzle[row][col]);
+					cols.get(col).add(puzzle[row][col]);
+					squares.get(row/3*3 + col/3).add(puzzle[row][col]);
 				}
 			}
 		}
-		return solve(0, 0, rows, cols, squares, this.board);
+		return solve(0, 0, rows, cols, squares, this.puzzle);
 	}
 	
-	private int[][] solve(int row, int col, List<Set<Integer>> rows, List<Set<Integer>> cols, List<Set<Integer>> squares, int[][] currentBoard) {
+	// helper method for solve() that recursively constructs a solution for the unsolved sudoku puzzle
+	private int[][] solve(int row, int col, List<Set<Integer>> rows, List<Set<Integer>> cols, List<Set<Integer>> squares, int[][] currentPuzzle) {
 		if (row == 9) {
-			return currentBoard;
+			return currentPuzzle;
 		} else if (col == 9) {
-			return solve(row + 1, 0, rows, cols, squares, currentBoard);
-		} else if (currentBoard[row][col] != 0) {
-			return solve(row, col + 1, rows, cols, squares, currentBoard);
+			return solve(row + 1, 0, rows, cols, squares, currentPuzzle);
+		} else if (currentPuzzle[row][col] != 0) {
+			return solve(row, col + 1, rows, cols, squares, currentPuzzle);
 		} else {
 			for (int i = 1; i < 10; i++) {
 				if (!rows.get(row).contains(i) && !cols.get(col).contains(i) && !squares.get(row/3*3 + col/3).contains(i)) {
-					currentBoard[row][col] = i;
+					currentPuzzle[row][col] = i;
 					rows.get(row).add(i);
 					cols.get(col).add(i);
 					squares.get(row/3*3 + col/3).add(i);
-					int[][] solution = solve(row, col + 1, rows, cols, squares, currentBoard);
+					int[][] solution = solve(row, col + 1, rows, cols, squares, currentPuzzle);
 					rows.get(row).remove(i);
 					cols.get(col).remove(i);
 					squares.get(row/3*3 + col/3).remove(i);
@@ -57,8 +60,71 @@ public class SudokuSolver {
 				}
 			}
 			
-			currentBoard[row][col] = 0;
+			currentPuzzle[row][col] = 0;
 			return null;
+		}
+	}
+	
+	// pre: difficulty must be between 0 and 10 and represents the desired difficulty of the sudoku puzzle
+	// post: returns a 2D array with the data for a random unsolved sudoku puzzle
+	public int[][] getRandomPuzzle(int difficulty) {
+		int[][] randomPuzzle = {{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0},
+								{0,0,0,0,0,0,0,0,0}};
+		
+		List<Set<Integer>> rows = new LinkedList<Set<Integer>>();
+		List<Set<Integer>> cols = new LinkedList<Set<Integer>>();
+		List<Set<Integer>> squares = new LinkedList<Set<Integer>>();
+		for (int i = 0; i < 9; i++) {
+			rows.add(new HashSet<Integer>());
+			cols.add(new HashSet<Integer>());
+			squares.add(new HashSet<Integer>());
+		}
+		return getRandomPuzzle(0, 0, rows, cols, squares, randomPuzzle, (double)difficulty/10.0);
+	}
+	
+	// helper method for getRandomPuzzle() that recursively constructs a random sudoku puzzle
+	private int[][] getRandomPuzzle(int row, int col, List<Set<Integer>> rows, List<Set<Integer>> cols, List<Set<Integer>> squares, int[][] currentPuzzle, double difficulty) {
+		if (row == 9) {
+			return currentPuzzle;
+		} else if (col == 9) {
+			return getRandomPuzzle(row + 1, 0, rows, cols, squares, currentPuzzle, difficulty);
+		} else if (currentPuzzle[row][col] != 0) {
+			return getRandomPuzzle(row, col + 1, rows, cols, squares, currentPuzzle, difficulty);
+		} else {
+			int[][] solution = null;
+			if (Math.random() > difficulty) {
+				while (solution == null) {
+					int randomNum;
+					int count = 0;
+					do {
+						if (count > 50) {
+							solution = null;
+						}
+						randomNum = (int)Math.round(Math.random()*8.0 + 1.0);
+						count++;
+					} while (rows.get(row).contains(randomNum) || cols.get(col).contains(randomNum) || squares.get(row/3*3 + col/3).contains(randomNum));
+					
+					currentPuzzle[row][col] = randomNum;
+					rows.get(row).add(randomNum);
+					cols.get(col).add(randomNum);
+					squares.get(row/3*3 + col/3).add(randomNum);
+					solution = getRandomPuzzle(row, col + 1, rows, cols, squares, currentPuzzle, difficulty);
+					rows.get(row).remove(randomNum);
+					cols.get(col).remove(randomNum);
+					squares.get(row/3*3 + col/3).remove(randomNum);
+				}
+			} else {
+				solution = getRandomPuzzle(row, col + 1, rows, cols, squares, currentPuzzle, difficulty);
+			}
+			
+			return solution;
 		}
 	}
 
@@ -78,9 +144,41 @@ public class SudokuSolver {
 		int[][] solution = solver.solve();
 		
 		if (solution != null) {
-			for (int i = 0; i < solution.length; i++) {
-				for (int e = 0; e < solution[i].length; e++) {
-					System.out.print(solution[i][e]);
+			for (int i = 0; i < 9; i++) {
+				for (int e = 0; e < 9; e++) {
+					System.out.print(solution[i][e] + (e == 8 ? "" : ","));
+				}
+				System.out.println();
+			}
+		} else {
+			System.out.println("No solution");
+		}
+		
+		System.out.println();
+		
+		int[][] random = solver.getRandomPuzzle(8);
+		
+		if (random != null) {
+			for (int i = 0; i < 9; i++) {
+				for (int e = 0; e < 9; e++) {
+					System.out.print(random[i][e] + (e == 8 ? "" : ","));
+				}
+				System.out.println();
+			}
+		} else {
+			System.out.println("No solution");
+		}
+		
+		System.out.println();
+		
+		SudokuSolver randomSolver = new SudokuSolver(random);
+		
+		int[][] randomSolution = randomSolver.solve();
+		
+		if (randomSolution != null) {
+			for (int i = 0; i < 9; i++) {
+				for (int e = 0; e < 9; e++) {
+					System.out.print(randomSolution[i][e] + (e == 8 ? "" : ","));
 				}
 				System.out.println();
 			}
